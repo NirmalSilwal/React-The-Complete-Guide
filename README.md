3183bc995ec2481e65fc78a859835775f2c316c3
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-07-01 18:15:32 -0500

    let, const, var testing

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # React-The-Complete-Guide
 Learning React from scratch
+<hr>
 
 Online Javascript platform to test your js code quickly - [click here](https://jsbin.com/?js,output)
---
+++ section2-JavascriptRefresher/variables.js
@@ -0,0 +1,15 @@
+var myName = "Nirmal Silwal"
+console.log(myName)
+
+let lettest = "Hello world"
+console.log(lettest)
+
+const consttest = "React learning"
+console.log(consttest)
+
+myName = "don"
+console.log(myName)
+
+// "TypeError: Assignment to constant variable.
+// consttest = "check"
+// console.log(consttest)
