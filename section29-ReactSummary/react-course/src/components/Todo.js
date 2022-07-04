652422b572719c6c29c711fcee141c8b9ddfc947
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-07-04 09:03:36 -0500

    Pushed last update- backdrop, modal and Todo js

--- section29-ReactSummary/react-course/src/components/Todo.js
+++ section29-ReactSummary/react-course/src/components/Todo.js
@@ -1,10 +1,32 @@
+import { useState } from "react";
+
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
 function Todo(props) {
+  const [modalIsOpen, setModelIsOpen] = useState(false);
+
+  function deleteHandler() {
+    setModelIsOpen(true);
+  }
+
+  function closeModalHandler() {
+    setModelIsOpen(false);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">DELETE</button>
+        <button className="btn" onClick={deleteHandler}>
+          DELETE
+        </button>
       </div>
+
+      {/* {modalIsOpen ? <Modal /> : null}  */}
+
+      {modalIsOpen && <Modal />}
+      {modalIsOpen && <Backdrop onClick={closeModalHandler} />}
     </div>
   );
 }
